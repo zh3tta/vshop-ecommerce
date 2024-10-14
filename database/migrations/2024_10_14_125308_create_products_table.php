f334f25c5ae0892e44e6a72761c4929c77a7d483
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-10-14 11:22:21 -0300

    feat: migrations added

--- database/migrations/2024_10_14_125308_create_products_table.php
+++ database/migrations/2024_10_14_125308_create_products_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +16,19 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 200);
+            $table->string('slug', 400);
+            $table->integer('quantity');
+            $table->longText('description')->nullable();
+            $table->boolean('published')->default(0);
+            $table->boolean('inStock')->default(0);
+            $table->decimal('price', 10,2);
+            $table->foreignIdFor(User::class, 'created_by')->nullable();
+            $table->foreignIdFor(User::class, 'updated_by')->nullable();
+            $table->foreignIdFor(Brand::class, 'brand_id')->nullable();
+            $table->foreignIdFor(Category::class, 'category_id')->nullable();
+            $table->softDeletes();
+            $table->foreignIdFor(User::class, 'deleted_by')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2024_10_14_125810_create_user_address_table.php
+++ database/migrations/2024_10_14_125810_create_user_address_table.php
@@ -11,8 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_address', function (Blueprint $table) {
+        Schema::create('user_addresses', function (Blueprint $table) {
             $table->id();
+            $table->string('type', 45);
+            $table->string('address1', 255);
+            $table->string('address2', 255)->nullable();
+            $table->string('city', 255);
+            $table->string('state', 45)->nullable();
+            $table->string('zipcode', 45);
+            $table->boolean('isMain')->default(1);
+            $table->string('country_code', 3);
+            $table->foreignId('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
