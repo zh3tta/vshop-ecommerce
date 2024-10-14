230d86c6c8e16f05c348b4b104ad2a47bdecef47
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-10-14 19:31:58 -0300

    fix: slug options of product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -26,7 +26,7 @@ class Product extends Model
     public function getSlugOptions() : SlugOptions
     {
         return SlugOptions::create()
-            ->generateSlugsFrom('name')
+            ->generateSlugsFrom('title')
             ->saveSlugsTo('slug');
     }
 }
