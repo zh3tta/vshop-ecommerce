b53a2b42610d33e0b0e54987b12f2dbe6d97d378
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-10-14 19:29:09 -0300

    feat: sluggable instaled

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use HasSlug;
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'published',
+        'inStock',
+        'price',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+    ];
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
