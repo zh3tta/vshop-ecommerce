798089782d6bc67c27d155fc4f1c71997c642cf2
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-10-15 15:04:21 -0300

    fix: sidebar link products

--- resources/js/Pages/Admin/Components/Sidebar.vue
+++ resources/js/Pages/Admin/Components/Sidebar.vue
@@ -86,7 +86,7 @@ import { Link } from '@inertiajs/vue3';
             <ul id="dropdown-products" class="hidden py-2 space-y-2">
               <li>
                 <Link
-                  :href="route(admin.products.index)"
+                  :href="route('admin.products.index')"
                   class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                   >List</Link
                 >
