f84d36497ed5e7c23d5c779863a9cceb91e97091
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-10-16 12:23:23 -0300

    feat: show product list

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,7 +11,8 @@ class ProductController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Admin/Product/Index');
+        $products = Product::get();
+        return Inertia::render('Admin/Product/Index', ['products' => $products]);
     }
 
     public function create()
