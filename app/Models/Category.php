657488073b31dba679b28a27b05f263bf132b076
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-10-17 11:26:07 -0300

    feat: models brand, category and product image

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
+    use HasSlug;
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+    
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
