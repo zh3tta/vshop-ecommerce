8059d002c4025b8cdf645cabe4fd19b2bae95d21
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-10-28 17:02:03 -0300

    feat: function open edit modal

--- resources/js/Pages/Admin/Product/ProductList.vue
+++ resources/js/Pages/Admin/Product/ProductList.vue
@@ -91,6 +91,16 @@ const resetFormData = () => {
 }
 
 const openEditModal = (product) => {
+    //updatde data
+    id.value = product.id;
+    title.value = product.title;
+    price.value = product.price;
+    quantity.value = product.quantity;
+    description.value = product.description;
+    brand_id.value = product.brand_id;
+    category_id.value = product.category_id;
+    product_images.value = product.product_images;
+
     isAddProduct.value = false
     dialogVisible.value = true
     editMode.value = true;
@@ -255,8 +265,18 @@ const openEditModal = (product) => {
                             <td class="px-4 py-3">{{ product.brand.name }}</td>
                             <td class="px-4 py-3">{{ product.quantity }}</td>
                             <td class="px-4 py-3">$ {{ product.price }}</td>
-                            <td class="px-4 py-3">{{ product.inStock }}</td>
-                            <td class="px-4 py-3">{{ product.published }}</td>
+                            <td class="px-4 py-3">
+                                <span v-if="product.inStock == 0"
+                                    class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">inStock</span>
+                                <span v-else
+                                    class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">Out of Stock</span>
+                            </td>
+                            <td class="px-4 py-3">
+                                <button v-if="product.published == 0" type="button"
+                                    class="px-3 py-2 text-xs font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Published</button>
+                                <button v-else type="button"
+                                    class="px-3 py-2 text-xs font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">UnPublished</button>
+                            </td>
                             <td class="px-4 py-3 flex items-center justify-end">
                                 <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
                                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
