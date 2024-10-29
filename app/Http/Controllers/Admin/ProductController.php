873df565b30abc49f6a3fc2de570d0490e53d684
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-10-29 16:24:55 -0300

    feat: function and route of update image

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -58,9 +58,29 @@ class ProductController extends Controller
         //
     }
 
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->description = $request->description;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+
+        if ($request->hasFile('product_images')) {
+            $productImages = $request->file('product_images');
+            foreach ($productImages as $image) {
+                $uniqueName = time() .'-'. Str::random(10) .'-'. $image->getClientOriginalExtension();
+                $image->move('product_images', $uniqueName);
+                ProductImage::create([
+                    'product_id' => $product->id,
+                    'image' => 'product_images/' . $uniqueName,
+                ]);
+            }
+        }
+        $product->update();
+        return redirect()->route('admin.products.index')->with('success', 'Product updated successfully.');
     }
 
     public function deleteImage($id)
--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     // Products
     Route::get('/products', [ProductController::class, 'index'])->name('admin.products.index');
     Route::post('/products/store', [ProductController::class, 'store'])->name('admin.products.store');
+    Route::put('/products/update/{id}', [ProductController::class, 'update'])->name('admin.products.update');
     Route::delete('/products/image/{id}', [ProductController::class, 'deleteImage'])->name('admin.products.image.delete');
 });
 
