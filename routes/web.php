153a27a91b88b3cd379f01a7b38b2062693113e1
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-10-30 19:02:04 -0300

    feat: user route home

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,12 @@ use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 // User routes
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [UserController::class, 'index'])->name('user.home');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
