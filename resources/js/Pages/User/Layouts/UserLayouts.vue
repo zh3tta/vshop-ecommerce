ec0e17cbbc76430cc398511190e7a11f115e1cd5
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-10-31 11:17:39 -0300

    feat: user home page ui setup

---
+++ resources/js/Pages/User/Layouts/UserLayouts.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted } from 'vue';
+import { initFlowbite } from 'flowbite';
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+import Hero from './Hero.vue';
+
+onMounted(() => {
+  initFlowbite();
+});
+</script>
+
+<template>
+  <!-- header -->
+  <Header></Header>
+  
+  <!-- hero section -->
+  <Hero></Hero>
+
+  <!-- main -->
+  <slot />
+
+  <!-- footer -->
+  <Footer></Footer>
+
+</template>
