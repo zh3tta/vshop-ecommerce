3f8d8352ac28a2823cb76fd2e65b3b7503c16257
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-04 19:38:11 -0300

    feat: cart items model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -47,4 +47,9 @@ class Product extends Model
     {
         return $this->belongsTo(Brand::class);
     }
+
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
