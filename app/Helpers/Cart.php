a188ade474663d9a6939b4c53950d58b44cd6bdf
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-05 18:27:09 -0300

    fix: get cart items

--- app/Helpers/Cart.php
+++ app/Helpers/Cart.php
@@ -13,6 +13,8 @@ class Cart
     {
         if ($user = auth()->user()) {
             return CartItem::whereUserId($user->id)->sum('quantity');
+        } else {
+            return array_reduce(self::getCookieCartItems(), fn ($carry) => $carry + 1, 0);
         }
     }
 
@@ -24,6 +26,8 @@ class Cart
                 'product_id' => $item->product_id,
                 'quantity' => $item->quantity
             ]);
+        } else {
+            return self::getCookieCartItems();
         }
     }
 
@@ -32,9 +36,9 @@ class Cart
         return json_decode(request()->cookie('cart_items', '[]'), true);
     }
 
-    public static function setCookieCartItems()
+    public static function setCookieCartItems(array $cartItems)
     {
-        Cookie::queue('cart_items', fn(int $carry, array $item) => $carry + $item['quantity'],0);
+        Cookie::queue('cart_items', json_encode($cartItems), 60*24*30);
     }
 
     public static function saveCookieCartItems()
