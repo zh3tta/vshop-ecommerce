eaaa851cfdaec2df2e7f707c14ba125509c0db35
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-08 16:29:14 -0300

    feat: filter price and category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -51,5 +52,21 @@ class Product extends Model
     public function cartItems()
     {
         return $this->hasMany(CartItem::class);
+    }
+
+    public function scopeFiltered(Builder $query)
+    {
+        $query->when(request('brands'), function (Builder $q) {
+            $q->whereIn('brand_id', request('brands'));
+        })
+        ->when(request('categories'), function (Builder $q) {
+            $q->whereIn('category_id', request('categories'));
+        })
+        ->when(request('prices'), function (Builder $q) {
+            $q->whereBetween('prices', [
+                request('prices.from', 0),
+                request('princes.to', 100000),
+            ]);
+        });
     }
 }
