45903fdf996c377ace9635f961d73618508cee84
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-09 15:10:58 -0300

    feat: checkout route added

---
+++ app/Http/Controllers/User/CheckoutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CheckoutController extends Controller
+{
+    public function store(Request $request)
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\User\CartController;
+use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\User\ProductListController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
@@ -21,6 +22,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('checkout')->controller(CheckoutController::class)->group(function () {
+        Route::post('order', 'store')->name('checkout.store');
+    });
 });
 
 // Admin routes
