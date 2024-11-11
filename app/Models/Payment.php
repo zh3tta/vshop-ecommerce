c3ce8ba0c53afc85ac4ba12ea98be2313efa9f0d
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-11 14:28:11 -0300

    feat: functions of payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['order_id', 'status', 'amount', 'type', 'created_by', 'updated_by'];
+
+    public function order()
+    {
+        return $this->hasOne(Order::class, 'id', 'order_id');
+    }
 }
