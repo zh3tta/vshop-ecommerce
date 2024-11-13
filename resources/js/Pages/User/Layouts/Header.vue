b927d2ced358e694130f754abf20a7a523e3ac93
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-13 15:28:59 -0300

    feat: route profile added

--- resources/js/Pages/User/Layouts/Header.vue
+++ resources/js/Pages/User/Layouts/Header.vue
@@ -47,7 +47,7 @@ const path = window.location.pathname;
           </div>
           <ul class="py-2" aria-labelledby="user-menu-button">
             <li>
-              <Link href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</Link>
+              <Link :href="route('profile.edit')" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</Link>
             </li>
             <li>
               <Link :href="route('purchases')" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Purchases</Link>
