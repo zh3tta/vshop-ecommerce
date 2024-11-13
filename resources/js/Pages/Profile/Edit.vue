95c505e9d88dcc4186217994630532cecc32c85c
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-13 15:23:06 -0300

    refactor: edit profile

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import UserLayouts from '../User/Layouts/UserLayouts.vue';
 import DeleteUserForm from './Partials/DeleteUserForm.vue';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
@@ -16,31 +17,28 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Profile" />
-
-    <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <UpdateProfileInformationForm
-                        :must-verify-email="mustVerifyEmail"
-                        :status="status"
-                        class="max-w-xl"
-                    />
-                </div>
-
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <UpdatePasswordForm class="max-w-xl" />
-                </div>
-
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <DeleteUserForm class="max-w-xl" />
-                </div>
-            </div>
+  <UserLayouts>
+    <!-- main -->
+    <div class="min-h-screen bg-gray-100">
+      <div class="pt-4 pb-24">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+          <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <UpdateProfileInformationForm
+                :must-verify-email="mustVerifyEmail"
+                :status="status"
+                class="max-w-xl"
+            />
+          </div>
+  
+          <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <UpdatePasswordForm class="max-w-xl" />
+          </div>
+  
+          <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <DeleteUserForm class="max-w-xl" />
+          </div>
         </div>
-    </AuthenticatedLayout>
+      </div>
+    </div>
+  </UserLayouts>
 </template>
