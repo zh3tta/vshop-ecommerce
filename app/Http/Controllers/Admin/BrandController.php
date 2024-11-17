215d4c1de73734f1e6a33a8c4f7b71fccaa8a9ba
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-17 11:48:09 -0300

    feat: admin brand controller

---
+++ app/Http/Controllers/Admin/BrandController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BrandController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+        
+    }
+}
