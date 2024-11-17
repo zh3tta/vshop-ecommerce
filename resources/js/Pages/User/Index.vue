7fe72033fc9bc07da750b02bf6544e1181f954c7
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-17 11:09:03 -0300

    refactor: product index styles

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -22,7 +22,7 @@ defineProps({
         <!-- product list component -->
         <Products :products="products"></Products>
         
-        <div class="flex justify-center mt-5">
+        <div class="flex justify-center mt-5 mb-8">
           <Link :href="route('products.index')" class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View All Products</Link>
         </div>
       </div>
