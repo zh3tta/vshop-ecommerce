c0c408f71b92f1bb08e5926487c123c8c82d56a0
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-18 15:46:43 -0300

    feat: show brand list

---
+++ resources/js/Pages/Admin/Brand/Index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AdminLayout from '../Components/AdminLayout.vue';
+import BrandList from './BrandList.vue';
+
+defineProps({
+  brands: Array
+});
+</script>
+
+<template>
+  <AdminLayout>
+    <BrandList :brands = "brands"></BrandList>
+  </AdminLayout>
+</template>
