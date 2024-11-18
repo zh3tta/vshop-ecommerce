825b5ef64ff4b8f408aa9b7c0e4405eaafff0c17
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-18 11:43:17 -0300

    feat: admin brand controller functions

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -3,27 +3,36 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class BrandController extends Controller
 {
     public function index()
     {
-
+        $brands = Brand::get();
+        return Inertia::render('Admin/Brand/Index', ['brands' => $brands]);
     }
 
     public function store(Request $request)
     {
-
+        $brand = new Brand($request->input());
+        $brand->save();
+        return redirect()->route('admin.brands.index')->with('success', 'Brand created successfully.');
     }
 
     public function update(Request $request, $id)
     {
-
+        $brand = Brand::findOrFail($id);
+        $brand->name = $request->name;
+        $brand->update();
+        return redirect()->route('admin.brands.index')->with('success', 'Brand updated successfully.');
     }
 
     public function destroy($id)
     {
-        
+        $brand = Brand::findOrFail($id)->delete();
+        return redirect()->route('admin.brands.index')->with('success', 'Brand deleted successfully.');
     }
 }
