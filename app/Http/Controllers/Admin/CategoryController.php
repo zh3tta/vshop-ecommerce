3d605e40532a01f08a246a9d2ddd8f5e5ed2e80f
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-19 12:24:52 -0300

    feat: admin category controller functions

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::get();
+        return Inertia::render('Admin/Category/Index', ['categories' => $categories]);
+    }
+
+    public function store(Request $request)
+    {
+        $category = new Category($request->input());
+        $category->save();
+        return redirect()->route('admin.categories.index')->with('success', 'Category created successfully.');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::findOrFail($id);
+        $category->name = $request->name;
+        $category->update();
+        return redirect()->route('admin.categories.index')->with('success', 'Category updated successfully.');
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id)->delete();
+        return redirect()->route('admin.categories.index')->with('success', 'Category deleted successfully.');
+    }
+}
