cc3147d8c6965bf1c8787a523347acd40e9cf537
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-19 14:10:07 -0300

    refactor: admin sidebar

--- resources/js/Pages/Admin/Components/Sidebar.vue
+++ resources/js/Pages/Admin/Components/Sidebar.vue
@@ -44,7 +44,7 @@ import { Link } from '@inertiajs/vue3';
           </li>
           <li>
             <Link
-              href="#"
+              :href="route('admin.brands.index')"
               class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <svg 
@@ -58,7 +58,7 @@ import { Link } from '@inertiajs/vue3';
           </li>
           <li>
             <Link
-              href="#"
+              :href="route('admin.categories.index')"
               class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <svg 
