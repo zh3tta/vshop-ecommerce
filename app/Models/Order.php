678c7b3561670b82130b139f19537c4e1fbb6e37
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-11-21 20:05:00 -0300

    feat: function user address in order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -15,4 +15,8 @@ class Order extends Model
     {
         return $this->hasMany(OrderItem::class);
     }
+
+    public function user_address(){
+        return $this->belongsTo(UserAddress::class);
+    }
 }
