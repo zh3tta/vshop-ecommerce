d2750c7db423a664f2207abac51acf08324a8947
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-12-04 14:51:50 -0300

    feat: home test

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    public function test_home_page_is_displayed(): void
+    {
+        $response = $this->get('/');
+        
+        $response->assertStatus(200);
+    }
+}
