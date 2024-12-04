1ec21ed9a540711fede2f49d1f85f16d64314696
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-12-04 15:12:50 -0300

    feat: purchase test

---
+++ tests/Feature/PurchaseTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PurchaseTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    public function test_purchase_page_is_displayed(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->get('/purchases');
+
+        $response->assertStatus(200);
+    }
+}
