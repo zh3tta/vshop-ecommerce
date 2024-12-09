42a5fed89ddbf0b733d4b399ebbd65ba2cd7a0b3
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-12-09 11:51:01 -0300

    feat: cart item factory

---
+++ database/factories/CartItemFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CartItem>
+ */
+class CartItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'product_id' => Product::all()->random()->id,
+            'quantity' => $this->faker->numberBetween(1, 19),
+        ];
+    }
+}
