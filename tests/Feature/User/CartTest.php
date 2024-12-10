3a4d80021d96929cbbb29fa913ddc4ecfe3c8636
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-12-10 16:18:42 -0300

    feat: test update and delete product to cart

--- tests/Feature/User/CartTest.php
+++ tests/Feature/User/CartTest.php
@@ -18,8 +18,8 @@ class CartTest extends TestCase
     public function test_cart_view_is_displayed(): void
     {
         Brand::factory(3)->create();
-        Category::factory(4)->create();
-        Product::factory(10)->create();
+        Category::factory(2)->create();
+        Product::factory(5)->create();
         $user = User::factory()->create();
         UserAddress::factory()->create();
         CartItem::factory()->create();
@@ -33,13 +33,43 @@ class CartTest extends TestCase
 
     public function test_add_product_to_cart(): void
     {
-        Brand::factory(2)->create();
-        Category::factory(2)->create();
+        Brand::factory()->create();
+        Category::factory()->create();
         $product = Product::factory()->create();
         $user = User::factory()->create();
 
         $this->actingAs($user);
         $response = $this->post('/cart/store/'. $product->id);
+
+        $response->assertRedirect();
+    }
+
+    public function test_update_product_to_cart(): void
+    {
+        Brand::factory()->create();
+        Category::factory()->create();
+        $product = Product::factory()->create();
+        $user = User::factory()->create();
+        CartItem::factory()->create();
+
+        $this->actingAs($user);
+        $response = $this->patch('/cart/update/'. $product->id, [
+            'quantity' => '2',
+        ]);
+
+        $response->assertRedirect();
+    }
+
+    public function test_remove_product_of_cart(): void
+    {
+        Brand::factory()->create();
+        Category::factory()->create();
+        $product = Product::factory()->create();
+        $user = User::factory()->create();
+        CartItem::factory()->create();
+
+        $this->actingAs($user);
+        $response = $this->delete('/cart/delete/'. $product->id);
 
         $response->assertRedirect();
     }
