904353cce8d256dd5d77c462b6e084a3ae942ee9
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-12-12 10:46:42 -0300

    feat: brand test

---
+++ tests/Feature/Admin/BrandTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Brand;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BrandTest extends TestCase
+{
+    use RefreshDatabase;
+    private $adminUser;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->adminUser = User::factory()->create([
+            'isAdmin' => 1,
+        ]);
+    }
+
+    public function test_brand_admin_page_is_displayed(): void
+    {
+        $response = $this->actingAs($this->adminUser)->get('/admin/brands');
+        
+        $response->assertStatus(200);
+    }
+
+    public function test_create_brand_page_is_displayed(): void
+    {
+        $response = $this->actingAs($this->adminUser)->post('/admin/brands/store', [
+            'name' => 'example',
+        ]);
+
+        $response->assertRedirect();
+    }
+
+    public function test_update_brand_page_is_displayed(): void
+    {
+        $brand = Brand::factory()->create();
+        $response = $this->actingAs($this->adminUser)
+            ->put('/admin/brands/update/'. $brand->id, [
+                'name' => 'update example',
+            ]);
+        
+        $response->assertRedirect();
+    }
+
+    public function test_delete_brand_page_is_displayed(): void
+    {
+        $brand = Brand::factory()->create();
+        $response = $this->actingAs($this->adminUser)
+            ->delete('/admin/brands/destroy/'. $brand->id);
+
+        $response->assertRedirect();
+    }
+
+}
