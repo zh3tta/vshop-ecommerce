605598001bf0439e1bbff26c04a698039ee5b85e
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-12-12 10:13:49 -0300

    feat: product image factory

---
+++ database/factories/ProductImageFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductImage>
+ */
+class ProductImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'product_id' => Product::all()->random()->id,
+            'image' => 'product_images/' . $this->faker->image('public/product_images', 640, 480, null, false),
+        ];
+    }
+}
